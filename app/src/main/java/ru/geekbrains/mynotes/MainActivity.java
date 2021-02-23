54c4243d3e8eb4b29c693b7db511df660cc6c293
AndrianNorth <disturbed7112@gmail.com>
2021-02-23 12:08:19 +0700

    Начало положено, но приложение падает при запуске

--- app/src/main/java/ru/geekbrains/mynotes/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mynotes/MainActivity.java
@@ -1,9 +1,11 @@
 package ru.geekbrains.mynotes;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = initToolbar();
+        initDrawerLayout(toolbar);
+    }
+
+    private void initDrawerLayout(Toolbar toolbar) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        actionBarDrawerToggle.syncState();
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
     }
 }
+    
